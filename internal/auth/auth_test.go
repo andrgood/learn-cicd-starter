89aedf9cc5896308152cdabf89448436e7bd0998
andrgood <goodboygoog@gmail.com>
2024-10-26 07:11:16 +1000

    fix test formatting

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,18 +8,18 @@ import (
 func TestGetAPIKey(t *testing.T) {
 
 	tests := map[string]struct {
-		input http.Header
-		want string
+		input   http.Header
+		want    string
 		wantErr bool
 	}{
-		"no auth handler": { input: http.Header{}, want: "", wantErr: true},
-		"split is less than 2": {input: http.Header{"Authorization": {"TESTFAIL"}}, want: "", wantErr: true},
-		"split first word is not ApiKey": {input:http.Header{"Authorization": {"TESTFAIL Bearer"}}, want: "", wantErr: true},
-		"return ApiKey": {input: http.Header{"Authorization": {"ApiKey TestKeyGotTested"}}, want: "TestKeyGotTested", wantErr: false},
+		"no auth handler":                {input: http.Header{}, want: "", wantErr: true},
+		"split is less than 2":           {input: http.Header{"Authorization": {"TESTFAIL"}}, want: "", wantErr: true},
+		"split first word is not ApiKey": {input: http.Header{"Authorization": {"TESTFAIL Bearer"}}, want: "", wantErr: true},
+		"return ApiKey":                  {input: http.Header{"Authorization": {"ApiKey TestKeyGotTested"}}, want: "TestKeyGotTested", wantErr: false},
 	}
 
 	for name, tc := range tests {
-		t.Run(name, func(t * testing.T) {
+		t.Run(name, func(t *testing.T) {
 			got, err := GetAPIKey(tc.input)
 			if (err != nil) != tc.wantErr {
 				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tc.wantErr)
